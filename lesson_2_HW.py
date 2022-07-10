270eea1a0e83f253e9dece8a970dfe86940d8ef1
gelono <63848074+gelono@users.noreply.github.com>
2022-07-10 14:45:01 +0300

    Add files via upload
    Home work for the lesson-2

---
+++ lesson_2_HW.py
@@ -0,0 +1,20 @@
+a = 2 + 3
+print(a)
+b = 3 - 2
+print(b)
+c = 3.1 * 2
+print(c)
+d = 3 / 2
+print(d)
+e = 9 // 2
+print(e)
+f = 9.1 % 5
+print(f)
+g = 3 ** 2
+print(g)
+i = -a
+print(i)
+str = "Hello" + " " + "World"
+print(str)
+my_list = [3 + 2, 8 / 2, "Hello", 2.5, "World"]
+print(my_list)
